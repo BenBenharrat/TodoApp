ab255fedc091d12794495bd96f9a03ba359bbbf9
BenBenharrat <benjaminbenharrat@gmail.com>
2019-11-21 11:18:22 +0100

    Link server config with dotenv variables

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+require("dotenv-extended").config();
+module.exports = {
+  "development": {
+    "username": process.env.USER_NAME,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
+    "dialect": "postgres",
+    "operatorsAliases": false
+  },
+}
