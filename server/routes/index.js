7054a502fc668808d8b370d229f88cc4b952064b
BenBenharrat <benjaminbenharrat@gmail.com>
2019-11-21 14:18:24 +0100

    link controller with new url routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
+const todosController = require("../controllers").todos;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+router.get("/api", (req, res) => {
+  res.status(200).send({
+    message: "Welcome to the Todos API"
+  });
+});
+
+router.post("/api/todos", todosController.create);
 
 module.exports = router;
