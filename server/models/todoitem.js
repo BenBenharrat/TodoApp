4cc7c094066c8bb1fde683e72fbf78a69aa0476a
BenBenharrat <benjaminbenharrat@gmail.com>
2019-11-21 12:20:32 +0100

    config models & associations

--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -1,11 +1,20 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const TodoItem = sequelize.define('TodoItem', {
-    content: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN
-  }, {});
+  const TodoItem = sequelize.define("TodoItem", {
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    complete: {
+      type: DataTypes.BOOLEAN,
+      defaulValue: false
+    }
+  });
   TodoItem.associate = function(models) {
-    // associations can be defined here
+    TodoItem.belongsTo(models.Todo, {
+      foreignKey: 'todoId',
+      onDelete: 'CASCADE'
+    });
   };
   return TodoItem;
 };
