ff202d02f86b95c9cb4cb712e4682cfdbc61b2d1
BenBenharrat <benjaminbenharrat@gmail.com>
2019-11-22 09:25:57 +0100

    add create todoItem feature

--- server/models/TodoItem.js
+++ server/models/TodoItem.js
@@ -1,11 +1,12 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
   const TodoItem = sequelize.define(
-    "TodoItem", 
+    "TodoItem",
     {
-    content: DataTypes.STRING,
-    complete: DataTypes.BOOLEAN
-    }
+      content: DataTypes.STRING,
+      complete: DataTypes.BOOLEAN
+    },
+    {}
   );
   TodoItem.associate = function(models) {
     TodoItem.belongsTo(models.Todo, {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,21 @@
 import express from "express";
 const router = express.Router();
 const todosController = require("../controllers").todos;
+const todoItemsController = require("../controllers").todoItems;
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/api", (req, res) => {
+router.get("/api", (req, res) =>
   res.status(200).send({
     message: "Welcome to the Todos API"
-  });
-});
+  })
+);
 
 router.get("/api/todos", todosController.list);
 router.post("/api/todos", todosController.create);
+router.post("/api/todos/:todoId/items", todoItemsController.create);
 
 module.exports = router;
